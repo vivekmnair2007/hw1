56c3ee6f71bc3a1948eed4c20bd8defbcf0fc807
Vivek Nair <vivek.nair@kellogg.northwestern.edu>
2025-01-28 04:31:36 +0000

    Added insert queries

--- kmdb.sql
+++ kmdb.sql
@@ -118,6 +118,7 @@ DROP TABLE IF EXISTS roles;
 
 -- Create new tables, according to your domain model
 --Table to store information about studios
+
 CREATE TABLE studios (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     name TEXT
@@ -145,7 +146,44 @@ CREATE TABLE roles (
 
 -- Insert data into your database that reflects the sample data shown above
 -- Use hard-coded foreign key IDs when necessary
--- TODO!
+
+INSERT INTO studios (name) VALUES
+('Warner Bros.');
+
+INSERT INTO movies (title, year_released, mpaa_rating, studio_id) VALUES
+('Batman Begins', 2005, 'PG-13', 1),
+('The Dark Knight', 2008, 'PG-13', 1),
+('The Dark Knight Rises', 2012, 'PG-13', 1);
+
+INSERT INTO actors (name) VALUES
+('Christian Bale'),
+('Michael Caine'),
+('Liam Neeson'),
+('Katie Holmes'),
+('Gary Oldman'),
+('Heath Ledger'),
+('Aaron Eckhart'),
+('Maggie Gyllenhaal'),
+('Tom Hardy'),
+('Joseph Gordon-Levitt'),
+('Anne Hathaway');
+
+INSERT INTO roles (actor_id, movie_id, character_name) VALUES
+(1, 1, 'Bruce Wayne'),
+(2, 1, 'Alfred'),
+(3, 1, 'Ra''s Al Ghul'),
+(4, 1, 'Rachel Dawes'),
+(5, 1, 'Commissioner Gordon'),
+(1, 2, 'Bruce Wayne'),
+(6, 2, 'Joker'),
+(7, 2, 'Harvey Dent'),
+(2, 2, 'Alfred'),
+(8, 2, 'Rachel Dawes'),
+(1, 3, 'Bruce Wayne'),
+(5, 3, 'Commissioner Gordon'),
+(9, 3, 'Bane'),
+(10, 3, 'John Blake'),
+(11, 3, 'Selina Kyle');
 
 -- Prints a header for the movies output
 .print "Movies"
